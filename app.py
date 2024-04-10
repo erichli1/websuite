cb80a6371065a3b445941de2876bc16f58d72677
erichli1 <ehli@college.harvard.edu>
2024-04-10 17:38:33 -0400

    Add basic logging of trajectories

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def get_data():
     data = {"message": "Hello from Flask!"}
     return jsonify(data)
+
+
+@app.route("/log/individual", methods=["POST"])
+def log_individual():
+    json = request.get_json()
+    log = json.get("log")
+
+    with open('trajectories/temp.txt', 'a') as file:
+        file.write(log + "\n")
+
+    return jsonify({"log": log})
 
 
 if __name__ == '__main__':
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/log/individual',
+            destination: 'http://127.0.0.1:5000/log/individual', // Proxy to Backend
+          },
+        ]
+    },
+};
 
 export default nextConfig;
